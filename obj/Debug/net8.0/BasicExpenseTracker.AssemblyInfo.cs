dafcca3d93cee336f596b56def1236b4b6c28978
Pritam7446 <ipritam746@gmail.com>
2026-05-28 12:44:02 +0530

    added entity freamework core

--- obj/Debug/net8.0/BasicExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/BasicExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e15855c97a7b5bc23a0abe9bc72d85d90e9c2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50f9c0f0cf2ab15f8e1815cd11fe309fd0d4b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
